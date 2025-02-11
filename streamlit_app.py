bc4037ac5d84bf97d2deb993e2eb113ea8fc1935
Sonia Ranvir <soniaranvir04@gmail.com>
2025-02-10 19:17:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,8 @@ st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout
 st.title("Sales Streamlit Dashboard")
 st.markdown("_Prototype v0.4.1_")
 
+all_months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
 #######################################
 # FILE PATH AND MONTHS LIST
 #######################################
